234c14fc65e086b8c4ba5dc8f0f3866584ac47a8
metin <klilikddfg@hotmail.de>
2017-05-20 23:07:41 +0200

    Revert "op3: Remove camera shim library"
    This reverts commit 299ad615e2c1c8aecfe9c5f7ec15208faed36a85.
    Brokes Camera...

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,4 +1,5 @@
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+#           (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,14 +17,10 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES := \
-    frameworks/av/include
-
-LOCAL_SRC_FILES := \
-    CameraParameters.cpp
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
-LOCAL_MODULE := libcamera_parameters_ext
+LOCAL_SRC_FILES := camera_shim.cpp
+LOCAL_MODULE := libcamera_shim
 LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_32_BIT_ONLY := true
 
-include $(BUILD_STATIC_LIBRARY)
+include $(BUILD_SHARED_LIBRARY)
